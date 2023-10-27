e69802832c862c9cc1e49d8c8b92b8c3acafc9fd
Yigit Uyumaz <yigituyumaz26@gmail.com>
2023-10-27 22:10:17 +0300

    refactor swapn

--- src/utils_swapn.c
+++ src/utils_swapn.c
@@ -3,14 +3,9 @@
 void
 utils_swapn(void *a, void *b, size_t n)
 {
-
-	unsigned char temp;
-
-	size_t i = 0;
-	while (i < n) {
-		temp = *((unsigned char *) a + i);
-		*((unsigned char *) a + i) = *((unsigned char *) b + i);
-		*((unsigned char *) b + i) = temp;
-		i++;
+	while (n--) {
+		*((unsigned char *) a + n) = *((unsigned char *) a + n) + *((unsigned char *) b + n);
+		*((unsigned char *) b + n) = *((unsigned char *) a + n) - *((unsigned char *) b + n);
+		*((unsigned char *) a + n) = *((unsigned char *) a + n) - *((unsigned char *) b + n);
 	}
 }
