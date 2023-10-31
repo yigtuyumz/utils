59d9a19696c70e6b2dcbc44da8c1140a65d31588
Yigit Uyumaz <yigituyumaz26@gmail.com>
2023-10-31 14:05:42 +0300

    xor swap

--- src/utils_swapn.c
+++ src/utils_swapn.c
@@ -4,8 +4,8 @@ void
 utils_swapn(void *a, void *b, size_t n)
 {
 	while (n--) {
-		*((unsigned char *) a + n) = *((unsigned char *) a + n) + *((unsigned char *) b + n);
-		*((unsigned char *) b + n) = *((unsigned char *) a + n) - *((unsigned char *) b + n);
-		*((unsigned char *) a + n) = *((unsigned char *) a + n) - *((unsigned char *) b + n);
+		*((unsigned char *) a + n) = *((unsigned char *) a + n) ^ *((unsigned char *) b + n);
+		*((unsigned char *) b + n) = *((unsigned char *) a + n) ^ *((unsigned char *) b + n);
+		*((unsigned char *) a + n) = *((unsigned char *) a + n) ^ *((unsigned char *) b + n);
 	}
 }
