fd21d1000c4ab34e62efdcba3ab7e07083c34cdc
Yigit Uyumaz <yigtuyumz@gmail.com>
2024-02-09 21:32:38 +0300

    fixed algo

--- src/utils_strncmp.c
+++ src/utils_strncmp.c
@@ -4,8 +4,12 @@ int
 utils_strncmp(const char *a, const char *b, size_t n)
 {
 	size_t i = 0;
-	while (*(a + i) && (*(a + i) - *(b + i) == 0) && i <= n) {
+	while (*(a + i) && (*(a + i) == *(b + i)) && i < n) {
 		i++;
+	}
+
+	if (i == n) {
+		return (0);
 	}
 
 	return (*(a + i) - *(b + i));
