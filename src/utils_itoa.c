da1fd269dd4df15fd185b6535d174c8bac4a7ea6
yigtuyumz <yigtuyumz@gmail.com>
2024-07-04 18:36:42 +0300

    removed comments

--- src/utils_itoa.c
+++ src/utils_itoa.c
@@ -1,9 +1,8 @@
 char *
 utils_itoa(int nb)
 {
-    // save buffer address between function calls with static keyword.
-    static char buffer[21]; // 19 int max, 1 for sign, 1 for null terminator.
-    char *p = buffer + 20;  // points to the end of buffer (null terminator.)
+    static char buffer[21];
+    char *p = buffer + 20;
 
     if (nb >= 0) {
         while (nb != 0) {
